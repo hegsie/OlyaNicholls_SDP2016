f7b7628cb42bc27d8240df50469b3fc3b1c2bbb8
olyagithub <olya.nicholls@hotmail.com>
2016-02-12 21:25:08 +0000

    vesion1.2

---
+++ Exercises/week_6/src/SquareAndSum.scala
@@ -0,0 +1,23 @@
+/**
+  * Created by ONicholls on 12/02/2016.
+  */
+import AtomicScala.AtomicTest1._
+
+object SquareAndSum extends App {
+
+  def squareThem(num: Int*):Int  = {
+    var sum  = 0
+    for(i <- num){
+
+      sum += i*i
+     }
+    sum
+
+  }
+  squareThem(2) is 4
+  squareThem(2, 4) is 20
+  squareThem(1, 2, 4) is 21
+
+}
+
+
