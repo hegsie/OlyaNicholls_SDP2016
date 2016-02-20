af8779911ad4c68f2aa6e6cae334a2c2e2801df0
olyagithub <olya.nicholls@hotmail.com>
2016-02-20 14:48:04 +0000

    vesion1

--- Exercises/week_7/src/MapAndReduce.scala
+++ Exercises/week_7/src/MapAndReduce.scala
@@ -1,8 +1,21 @@
-/**
-  * Created by ONicholls on 17/02/2016.
-  */
+import atomicscala.AtomicTest._
+
+
 object MapAndReduce extends App{
 
-  println("It is going to be fine!")
+  val v = Vector(1, 2, 3, 4)
+  v.map(n => n*11+10) is Vector(21,32,43,54)
+
+  val v1 = Vector(1, 2, 3, 4)
+  val newV = v1.foreach(n => n*11+10) is Vector(21,32,43,54)
+
+  newV is Vector(21,32,43,54)
+
+
 
 }
+
+
+//val v = Vector(1, 10 , 100, 1000)
+//5 var sum = 0
+//6 v.foreach(x => sum += x)
